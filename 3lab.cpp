d3e51a1425702654d91279bb92e545cd70a6c97f
Aylon <48117029+Aylon28@users.noreply.github.com>
2020-09-30 01:13:54 +0300

    Add files via upload

---
+++ 3lab.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+class el
+{
+	int a;
+
+	public:	
+		el() {};
+		el(int A) {
+			a = A;
+		};
+		void print();
+		void operator++(int);
+		static void returnlink(el);
+};
+
+void el::operator++(int) {
+	a++;
+}
+
+void el::returnlink(el k) {
+	std::cout << "\nСсылка а = " << &k;
+}
+
+void el::print() {
+	std::cout << "Значение а = " << a;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int val;
+
+	std::cout << "Введите значение = ";
+	std::cin >> val;
+	el element(val);
+	
+	element++;
+	element.print();
+	el::returnlink(element);
+	
+	std::cout << "\n\n";
+}
